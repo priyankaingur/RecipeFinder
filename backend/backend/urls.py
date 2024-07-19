f23cf72dc02900a258d023909746ad2b16a9949c
priyankaingur <priyankaingur@gmail.com>
2024-07-18 18:13:37 -0700

    updating uri for fetching nutrients

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/recipes/', RapidApiRecipeSearch.as_view(), name='rapidapi-recipe-search'),
     path('api/recipes/<int:recipe_id>/', RapidApiRecipeDetail.as_view(), name='rapidapi-recipe-detail'),
-    path('api/recipes/<int:recipe_id>/',
+    path('api/recipes/<int:recipe_id>/nutrients',
          RapidApiRecipeNutrients.as_view(), name='rapidapi-recipe-nutrients'),
     path('api/recipes/log-view/', CreateRecipeView.as_view(), name='log-view'),
     path('api/recipes/most-viewed/', GetMostViewedRecipesView.as_view(), name='most-viewed'),
